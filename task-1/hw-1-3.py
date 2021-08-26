a93c6273aac8eaa579060a0cd01cc0b7692e6859
MK <m.kovalev1988@gmail.com>
2021-08-26 13:46:11 +0000

    add first hw

---
+++ task-1/hw-1-3.py
@@ -0,0 +1,7 @@
+for i in range (1, 100):
+    if i % 10  == 1:
+        print (i, " procent")
+    elif i % 10 in [2, 3, 4]:
+        print (i, ' procenta')
+    else:
+        print (i, ' procentov')
